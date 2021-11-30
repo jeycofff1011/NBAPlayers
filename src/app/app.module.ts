9b2cf3b53baeb1d24746e0db5b8eb7bc896e0932
jeycoff1011 <realjaco1011@gmail.com>
2021-11-29 21:46:32 -0500

    estilizando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,26 @@ import { AppComponent } from './app.component';
 import { JsonService } from 'src/services/json.service';
 import { BuscarComponent } from './buscar/buscar.component';
 import { FormsModule } from '@angular/forms';
+import { MatListModule} from '@angular/material/list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component'
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     BuscarComponent,
+    HeaderComponent,
    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    MatListModule,
+    BrowserAnimationsModule
   ],
   providers: [JsonService],
   bootstrap: [AppComponent]
